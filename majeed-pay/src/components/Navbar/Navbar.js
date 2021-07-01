d4c7b181937ea475648dddffb58fea4069936262
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-07-01 23:45:09 +0300

    made the navbar mobile friendly

--- majeed-pay/src/components/Navbar/Navbar.js
+++ majeed-pay/src/components/Navbar/Navbar.js
@@ -1,29 +1,39 @@
 import React from 'react';
-
+import MenuBtn from './MenuBtn.svg';
 import Btn from '../Btn';
 import Logo from './Logo';
+import Xbtn from './Xbtn.svg';
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <nav>
       <Logo />
 
       <div className='navList'>
-        <ul className='navbar-links'>
-          <li className='list-item'>
+        <ul>
+          <li>
+            <img className='Xbtn' src={Xbtn} alt='' />
+          </li>
+          <li>
             <a href=''>Pricing</a>
           </li>
-          <li className='list-item'>
+          <li>
             <a href=''>About</a>
           </li>
-          <li className='list-item'>
+          <li>
             <a href=''>Contact</a>
+          </li>
+          <li>
+            <Btn btnType='btn-1' btnText='Schedule a Demo' />
           </li>
         </ul>
       </div>
       <div className='navBtn'>
         <Btn btnType='btn-1' btnText='Schedule a Demo' />
       </div>
-    </div>
+      <div className='menu-btn'>
+        <img src={MenuBtn} alt='' />
+      </div>
+    </nav>
   );
 };
 
