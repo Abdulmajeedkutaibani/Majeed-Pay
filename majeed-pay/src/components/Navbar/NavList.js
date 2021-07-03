cd2f68a657f777a9ff038ca4905819ec48f280ff
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-07-03 04:05:38 +0300

    finished navbar component and made it mobile friendly

---
+++ majeed-pay/src/components/Navbar/NavList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Btn from '../Btn';
+import { useState } from 'react';
+
+const NavList = () => {
+  const closeMenu = () => {
+    return (document.getElementById('navList').style.display = 'none');
+  };
+  return (
+    <div id='navList' className='navList'>
+      <ul>
+        <div className='close-icon' onClick={closeMenu}>
+          <div className='line-1'></div>
+          <div className='line-2'></div>
+        </div>
+        <div className='bottom-line'></div>
+        <li>
+          <a href=''>Pricing</a>
+        </li>
+        <li>
+          <a href=''>About</a>
+        </li>
+        <li>
+          <a href=''>Contact</a>
+        </li>
+        <li>
+          <Btn btnType='btn-1' btnText='Schedule a Demo' />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavList;
--- majeed-pay/src/components/Navbar/Navbar.js
+++ majeed-pay/src/components/Navbar/Navbar.js
@@ -2,36 +2,21 @@ import React from 'react';
 import MenuBtn from './MenuBtn.svg';
 import Btn from '../Btn';
 import Logo from './Logo';
-import Xbtn from './Xbtn.svg';
+import NavList from './NavList';
+
 const Navbar = () => {
+  const displayMenu = () => {
+    return (document.getElementById('navList').style.display = 'block');
+  };
   return (
     <nav>
       <Logo />
-
-      <div className='navList'>
-        <ul>
-          <li>
-            <img className='Xbtn' src={Xbtn} alt='' />
-          </li>
-          <li>
-            <a href=''>Pricing</a>
-          </li>
-          <li>
-            <a href=''>About</a>
-          </li>
-          <li>
-            <a href=''>Contact</a>
-          </li>
-          <li>
-            <Btn btnType='btn-1' btnText='Schedule a Demo' />
-          </li>
-        </ul>
+      <div className='menu-btn' onClick={displayMenu}>
+        <img src={MenuBtn} alt='' />
       </div>
+      <NavList />
       <div className='navBtn'>
         <Btn btnType='btn-1' btnText='Schedule a Demo' />
-      </div>
-      <div className='menu-btn'>
-        <img src={MenuBtn} alt='' />
       </div>
     </nav>
   );
